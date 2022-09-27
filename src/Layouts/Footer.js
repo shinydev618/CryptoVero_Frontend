31733cf340c43268015805ccef75471081d2686e
shinydev618 <shinydev618@gmail.com>
2022-09-27 04:18:28 -0400

    implemented project structure

---
+++ src/Layouts/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Box } from "@mui/material";
+import styled from "styled-components";
+
+const Footer = () => {
+  return <StyledComponent></StyledComponent>;
+};
+
+const StyledComponent = styled(Box)`
+  display: flex;
+  width: 100%;
+`;
+
+export default Footer;

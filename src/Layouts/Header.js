08eb3112462711adc7a5852c65df97b4315c5da3
shinydev618 <shinydev618@gmail.com>
2022-09-29 21:52:54 -0400

    updated header

--- src/Layouts/Header.js
+++ src/Layouts/Header.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box } from "@mui/material";
 import styled from "styled-components";
 import imgLogo01 from "../Assets/Images/mark/logo01.png";
 
 const Header = () => {
+  const [flagLanguage, setFlagLanguage] = useState(false);
   return (
     <StyledComponent>
       <PartMax01>
@@ -15,7 +16,22 @@ const Header = () => {
           <LinkEach01>Pricing</LinkEach01>
           <LinkEach01>Sign in</LinkEach01>
           <LinkEach01>Get Started for Free</LinkEach01>
-          <ButtonSwitchLanguage01></ButtonSwitchLanguage01>
+          <ButtonSwitchLanguage01>
+            <Box display={"flex"} width="100%" height="100%">
+              <ButtonLeft01
+                onClick={() => setFlagLanguage(false)}
+                flag={flagLanguage ? 1 : 0}
+              >
+                FIN
+              </ButtonLeft01>
+              <ButtonRight01
+                onClick={() => setFlagLanguage(true)}
+                flag={flagLanguage ? 1 : 0}
+              >
+                ENG
+              </ButtonRight01>
+            </Box>
+          </ButtonSwitchLanguage01>
         </PartLink01>
       </PartMax01>
     </StyledComponent>
@@ -74,10 +90,63 @@ const LinkEach01 = styled(Box)`
 `;
 
 const ButtonSwitchLanguage01 = styled(Box)`
+  display: flex;
   width: 120px;
   height: 38px;
+  background: #ffffff;
   box-shadow: 0px 12px 33px rgba(69, 62, 194, 0.25);
   border-radius: 10000px;
+  padding: 3px;
+  box-sizing: border-box;
+  align-items: center;
+`;
+
+const ButtonLeft01 = styled(Box)`
+  display: flex;
+  width: 60px;
+  height: 100%;
+  color: ${({ flag }) => (flag ? "#005e95" : "#ffffff")};
+  background: ${({ flag }) => (flag ? "white" : "#005e95")};
+  border-radius: 10000px;
+  justify-content: center;
+  align-items: center;
+  font-family: "Outfit";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 28px;
+  /* identical to box height, or 175% */
+
+  text-align: center;
+  letter-spacing: -0.015em;
+  text-transform: uppercase;
+  cursor: pointer;
+
+  transition: .3s;
+`;
+
+const ButtonRight01 = styled(Box)`
+  display: flex;
+  flex: 1;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  font-family: "Outfit";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 28px;
+  /* identical to box height, or 175% */
+
+  text-align: center;
+  letter-spacing: -0.015em;
+  text-transform: uppercase;
+  color: ${({ flag }) => (!flag ? "#005e95" : "#ffffff")};
+  background: ${({ flag }) => (!flag ? "white" : "#005e95")};
+  border-radius: 10000px;
+  cursor: pointer;
+
+  transition: .3s;
 `;
 
 export default Header;

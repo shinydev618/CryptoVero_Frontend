3aed7025977a356dca4ca0521a81d396d2e961e2
shinydev618 <shinydev618@gmail.com>
2022-10-10 09:44:39 -0400

    update taxes page

--- src/Pages/Tax/Taxes.js
+++ src/Pages/Tax/Taxes.js
@@ -5,6 +5,7 @@ import { dataTaxStatic } from "../../Config/data";
 import CustomStatic from "../../Components/Tax/CustomStatic";
 import { TextField } from "@mui/material";
 import MenuItem from "@mui/material/MenuItem";
+import CustomMainButton from "../../Components/CustomMainButton";
 
 const Taxes = () => {
   const [flagSelectYear, setFlagSelectYear] = useState("2022");
@@ -36,6 +37,48 @@ const Taxes = () => {
           return <CustomStatic key={index} data={each} />;
         })}
       </PartStatic01>
+      <PartContent01>
+        <PartContentLeft01>
+          <PartHeaderTransaction01>
+            <TextContentTitle02>2021 Tax Forms</TextContentTitle02>
+            <PartRightContent01>
+              <ButtonLearnMore01>
+                <CustomMainButton
+                  width={"100%"}
+                  height={"100%"}
+                  text={"learn more"}
+                  backColor={"white"}
+                  color={"#005E95"}
+                  borderRadius={"100px"}
+                  border={"1px solid #005E95"}
+                  fontSize={"16px"}
+                />
+              </ButtonLearnMore01>
+            </PartRightContent01>
+          </PartHeaderTransaction01>
+          <PartContentDown01></PartContentDown01>
+        </PartContentLeft01>
+        <PartContentRight01>
+          <PartHeaderTransaction01>
+            <TextContentTitle02>Cost Basis Method</TextContentTitle02>
+            <PartRightContent01>
+              <ButtonLearnMore01>
+                <CustomMainButton
+                  width={"100%"}
+                  height={"100%"}
+                  text={"learn more"}
+                  backColor={"white"}
+                  color={"#005E95"}
+                  borderRadius={"100px"}
+                  border={"1px solid #005E95"}
+                  fontSize={"16px"}
+                />
+              </ButtonLearnMore01>
+            </PartRightContent01>
+          </PartHeaderTransaction01>
+          <PartContentDown01></PartContentDown01>
+        </PartContentRight01>
+      </PartContent01>
     </StyledComponent>
   );
 };
@@ -76,4 +119,71 @@ const PartStatic01 = styled(Box)`
   gap: 40px;
   align-items: center;
 `;
+
+const PartContent01 = styled(Box)`
+  display: flex;
+  width: 100%;
+  margin-top: 50px;
+  gap: 20px;
+  justify-content: space-between;
+`;
+
+const PartContentLeft01 = styled(Box)`
+  display: flex;
+  flex: 1;
+  width: 100%;
+  border-radius: 20px;
+  border: 1px solid rgba(113, 111, 150, 0.2);
+  flex-direction: column;
+`;
+
+const PartContentRight01 = styled(Box)`
+  display: flex;
+  flex: 1;
+  width: 100%;
+  border-radius: 20px;
+  border: 1px solid rgba(113, 111, 150, 0.2);
+  flex-directiocolumnn
+`;
+
+const PartHeaderTransaction01 = styled(Box)`
+  display: flex;
+  width: 100%;
+  height: 95px;
+  padding: 0px 60px;
+  box-sizing: border-box;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid rgba(113, 111, 150, 0.2);
+`;
+
+const TextContentTitle02 = styled(Box)`
+  display: flex;
+  font-family: "Outfit";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 28px;
+  line-height: 35px;
+  text-transform: capitalize;
+  font-feature-settings: "pnum" on, "lnum" on;
+
+  color: #0d1942;
+`;
+
+const PartRightContent01 = styled(Box)`
+  display: flex;
+  align-items: center;
+`;
+
+const ButtonLearnMore01 = styled(Box)`
+  display: flex;
+  width: 155px;
+  height: 40px;
+`;
+
+const PartContentDown01 = styled(Box)`
+  display: flex;
+  width: 100%;
+`;
+
 export default Taxes;

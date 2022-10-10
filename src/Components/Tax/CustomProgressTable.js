42cd9d1cbf409c386a0477e2949b0fa1afb7f52c
shinydev618 <shinydev618@gmail.com>
2022-10-10 08:21:28 -0400

    completed holding page of tax

--- src/Components/Tax/CustomProgressTable.js
+++ src/Components/Tax/CustomProgressTable.js
@@ -6,8 +6,47 @@ import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableRow from "@mui/material/TableRow";
+import PropTypes from "prop-types";
+import LinearProgress from "@mui/material/LinearProgress";
+import Typography from "@mui/material/Typography";
+
+function LinearProgressWithLabel(props) {
+  return (
+    <Box sx={{ display: "flex", alignItems: "center" }}>
+      <Box sx={{ width: "100%", mr: 1 }}>
+        <LinearProgress variant="determinate" {...props} />
+      </Box>
+      <Box sx={{ minWidth: 35 }}>
+        <Typography variant="body2" color="#716F96">{`${Math.round(
+          props.value
+        )}%`}</Typography>
+      </Box>
+    </Box>
+  );
+}
+
+LinearProgressWithLabel.propTypes = {
+  /**
+   * The value of the progress indicator for the determinate and buffer variants.
+   * Value between 0 and 100.
+   */
+  value: PropTypes.number.isRequired,
+};
 
 const CustomProgressTable = ({ data }) => {
+  const [progress, setProgress] = React.useState(10);
+
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((prevProgress) =>
+        prevProgress >= 100 ? 10 : prevProgress + 10
+      );
+    }, 800);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
     <TableContainer width={"100%"}>
       <Table sx={{ minWidth: 1024 }}>
@@ -24,7 +63,7 @@ const CustomProgressTable = ({ data }) => {
               <TableCell align="left" width={"12px"}>
                 {""}
               </TableCell>
-              <TableCell align="left">
+              <TableCell align="left" width={"70px"}>
                 <BoxIconProgress01></BoxIconProgress01>
               </TableCell>
               <TableCell align="left">
@@ -33,10 +72,14 @@ const CustomProgressTable = ({ data }) => {
                     <TextProgressEthBig>Ethereum</TextProgressEthBig>
                     <TextProgressEthSmall>(ETH)</TextProgressEthSmall>
                   </TextUpTransfter01>
-                  <TextDownTransfter01>Coinbase</TextDownTransfter01>
+                  <TextDownTransfter01>
+                    <Box sx={{ width: "100%" }}>
+                      <LinearProgressWithLabel value={progress} />
+                    </Box>
+                  </TextDownTransfter01>
                 </BoxProgress01>
               </TableCell>
-              <TableCell align="left">
+              <TableCell align="left" width={"70px"}>
                 <BoxCurrency01>
                   <TextUpCurrency01>ETH</TextUpCurrency01>
                   <TextDownCurrency01>USD</TextDownCurrency01>
@@ -60,7 +103,7 @@ const BoxIconProgress01 = styled(Box)`
   background: #005e95;
   opacity: 0.1;
   border-radius: 100%;
-  margin-right: 20px;
+  /* margin-right: 20px; */
 `;
 
 const BoxProgress01 = styled(Box)`
@@ -137,7 +180,6 @@ const TextDownCurrency01 = styled(Box)`
   color: #716f96;
 `;
 
-
 const TextProgressEthBig = styled(Box)`
   display: flex;
   margin-right: 10px;
@@ -169,4 +211,5 @@ const TextProgressEthSmall = styled(Box)`
 
   color: #716f96;
 `;
+
 export default CustomProgressTable;

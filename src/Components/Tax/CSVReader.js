82e6254cc632b5c14731bde17845089fbeffc2da
shinydev618 <shinydev618@gmail.com>
2022-10-18 01:52:16 -0400

    read data from csv

--- src/Components/Tax/CSVReader.js
+++ src/Components/Tax/CSVReader.js
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box } from "@mui/material";
 import styled from "styled-components";
 
 const CSVReader = () => {
+  const [file, setFile] = useState();
+  const fileReader = new FileReader();
+
+  const fileSelected = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const readCSV = (e) => {
+    e.preventDefault();
+    if (file) {
+      fileReader.onload = function (event) {
+        const csvOutput = event.target.result;
+        console.log(csvOutput);
+      };
+      fileReader.readAsText(file);
+    }
+  };
+
   return (
     <StyledComponent>
       <form>
-        <input type={"file"} accept={".csv"} />
-        <button>IMPORT CSV</button>
+        <input
+          type={"file"}
+          id={"csvFileInput"}
+          accept={".csv"}
+          onChange={fileSelected}
+        />
+
+        <button
+          onClick={(e) => {
+            readCSV(e);
+          }}
+        >
+          Read
+        </button>
       </form>
     </StyledComponent>
   );
